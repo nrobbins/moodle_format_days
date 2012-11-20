afa36d78ae0a3ed23e92e75dedde8e4235318baa
Nathan Robbins <nirobbins@navitor.com>
2012-11-20 10:06:10 -0500

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012111400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2012112000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012110900.00;        // Requires this Moodle version.
 $plugin->component = 'format_days';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.0"; // User-friendly version number
+$plugin->release   = "3.0.1"; // User-friendly version number
